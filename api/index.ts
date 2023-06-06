f4dab4a9eb1bb3d700e41b65a7e31af1ea1d4825
Bidesh <bideshbanerjee444@gmail.com>
2023-06-06 18:03:48 +0530

    deploy check

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,21 @@
-import app from '../src/app'
+import appRoot from '../src/app'
+
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+
+// Instantiate Fastify with some config
+const app = Fastify({
+  logger: true
+});
+
+// Register your application as a normal plugin.
+app.register(appRoot);
+
+module.exports = async (req:FastifyRequest, res:FastifyReply) => {
+  await app.ready();
+  app.server.emit("request", req, res);
+};
+
+
 
 module.exports = app;
 // Register your application as a normal plugin.
